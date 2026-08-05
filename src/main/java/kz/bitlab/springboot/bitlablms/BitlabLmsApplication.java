a399a6ba64bca3ef55d5a5531992323d32191d91
Iskander Idilov <idiloviskander@gmail.com>
2026-08-06 01:12:02 +0500

    init

---
+++ src/main/java/kz/bitlab/springboot/bitlablms/BitlabLmsApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.springboot.bitlablms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitlabLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitlabLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/bitlab/springboot/bitlablms/BitlabLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.springboot.bitlablms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitlabLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
